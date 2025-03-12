d923fb26af98032ef550009a0e3e18f7f7fdf97b
KapilMP <kapilpradhan77ik@gmail.com>
2025-03-12 11:42:44 +0545

    Simple library api to convert model into json format

---
+++ code/apis/views.py
@@ -0,0 +1,13 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from books.models import Book
+from .serializers import BookSerializer
+
+
+class BookAPIView(APIView):
+    def get(self,request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many = True)
+        return Response(serializer.data)
